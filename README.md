57dc7ba83d09496747af872f894ca5d2d94dc158
lll2yu <lll2yu@protonmail.com>
2017-08-04 21:09:49 +0530

    add connection configuration script

--- README.md
+++ README.md
@@ -16,5 +16,8 @@ It is a parking terminal software implemented in java and database is handled by
 <h1>Server Configuration</h1>
 <a href="https://github.com/LLL2yu/parkalot/blob/master/server%20configuration">Server configuration</a>
 <br>
+<h1>Build Instructions</h1>
+<p>Run script configure_connection.sh in /src/parkalot/config to add your connection info to the code</p>
+<p>Build the project and run</p>
 <h1>Downloads</h1>
 <a href="https://github.com/LLL2yu/parkalot/releases">Releases can be found here</a>
---
+++ src/parkalot/config/configure_connection.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+cd ..
+echo "This will help you configure your source with your sql connection"
+echo ""
+echo "****************************************"
+echo "*     connection configuration          *"
+echo "* if this is your first run press 1     *"
+echo "*            else press 2	        *"
+echo "****************************************"
+echo ""
+read tin
+if [ $tin = "1" ]
+	then
+	echo "enter your JDBC driver name like(e.g. org.mariadb.jdbc.Driver,etc)"
+	read conDriver
+	echo "Your entered driver name = $conDriver"
+	echo "you want to continue? Y/n"
+	read response
+		if [ $response = "y" ]
+			then
+			echo "replacing the code......"
+			sed -i -- "s|add your jdbc Driver|$conDriver|g" *.java
+		else
+		echo "exiting......."
+		exit 0
+	fi
+	
+	echo "enter your connection info "
+	echo "##refer to your JDBC driver documentation for information on setting up connections"
+	#  refer to your JDBC driver documentation for information on setting up connections	
+	read conInfo
+	echo "Your entered connection info = $conInfo"
+	echo "you want to continue? Y/n"
+	read response2
+		if [ $response2 = "y" ]
+			then
+			echo "replacing the code......"
+			sed -i -- "s|add your connectin info|$conInfo|g" *.java
+		else
+		echo "exiting......."
+		exit 0
+	fi
+else
+	echo "enter your previous JDBC driver name "
+	read prevDriver
+	echo "enter your new JDBC driver name like(e.g. org.mariadb.jdbc.Driver,etc)"
+	read conDriver
+	echo "Your entered driver name = $conDriver"
+	echo "you want to continue? Y/n"
+	read response
+		if [ $response = "y" ]
+			then
+			echo "replacing the code......"
+			sed -i -- "s|$prevDriver|$conDriver|g" *.java
+		else
+		echo "exiting......."
+		exit 0
+	fi
+	
+	echo "enter your previous connection info "
+	read prevInfo
+	echo "enter your new connection info "
+	echo "##refer to your JDBC driver documentation for information on setting up connections"
+	#  refer to your JDBC driver documentation for information on setting up connections	
+	read conInfo
+	echo "Your entered connection info = $conInfo"
+	echo "you want to continue? Y/n"
+	read response2
+		if [ $response2 = "y" ]
+			then
+			echo "replacing the code......"
+			sed -i -- "s|$prevInfo|$conInfo|g" *.java
+		else
+		echo "exiting......."
+		exit 0
+	fi
+fi
+echo "**********************************************"
+echo "*                      Done                  *"
+echo "*                                                                     *"
+echo "**********************************************"
+exit 0
