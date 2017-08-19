8d68118bf01c5658bceae104db8a1ee2efd1ed3f
lll2yu <lll2yu@protonmail.com>
2017-08-19 21:21:31 +0530

    add a couple new features

--- src/parkalot/emp_show.java
+++ src/parkalot/emp_show.java
@@ -16,6 +16,11 @@
  */
 package parkalot;
 
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author lll2yu
@@ -26,9 +31,50 @@ public class emp_show extends javax.swing.JFrame {
      * Creates new form emp_show
      */
     public emp_show() {
+        user_sorter();
+        add_box();
         initComponents();
     }
-
+    void add_box(){
+        cb1.setModel(new DefaultComboBoxModel<String>(list.toArray(new String[0])));
+        getContentPane().add(cb1);
+        cb1.setBounds(530, 180, 270, 27);
+    }
+    ArrayList<String> list=new ArrayList<String>();
+    ArrayList<String> details=new ArrayList<String>();
+    void user_sorter(){
+        ResultSet rlt;
+        try{
+			Class.forName("add your jdbc Driver");
+			Connection c=DriverManager.getConnection("add your connectin info");
+			Statement stat=c.createStatement();
+			rlt=stat.executeQuery("Select login from user_details ");			
+ 				while (rlt.next()) {      
+  					 list.add((rlt.getString(1)));                                 
+				}
+		}
+		catch(Exception e){}
+}
+    
+    void get_details(){
+        String name = null;
+        Object sI=cb1.getSelectedItem();
+        if (sI != null)
+			{
+    				name= sI.toString();
+                        }
+        ResultSet rlt;
+        try{
+			Class.forName("add your jdbc Driver");
+			Connection c=DriverManager.getConnection("add your connectin info");
+			Statement stat=c.createStatement();
+			rlt=stat.executeQuery("Select * from user_details where login='"+name+"'");			
+ 				while (rlt.next()) {      
+  					 details.add((rlt.getString(1)));                                 
+				}
+		}
+		catch(Exception e){}
+}
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -38,21 +84,69 @@ public class emp_show extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBounds(new java.awt.Rectangle(240, 90, 0, 0));
+        setMinimumSize(new java.awt.Dimension(900, 640));
+        setPreferredSize(new java.awt.Dimension(900, 640));
+        setResizable(false);
+        getContentPane().setLayout(null);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Full Name", "Username", "Address", "Password"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(120, 400, 690, 100);
+
+        jButton1.setText("Get Info");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(420, 280, 100, 29);
+
+        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 14)); // NOI18N
+        jLabel1.setText("Display Information of Employees");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(320, 80, 270, 30);
+
+        jLabel2.setText("Select the user to show info of:");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(150, 180, 187, 30);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/if_group_318580.png"))); // NOI18N
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(440, 30, 48, 48);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/frame_bg.png"))); // NOI18N
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 0, 900, 640);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        DefaultTableModel model=(DefaultTableModel) jTable1.getModel();
+        get_details();
+        Object[] row= details.toArray();
+        model.addRow(row);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,5 +184,13 @@ public class emp_show extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
+    private javax.swing.JComboBox<String> cb1 = new javax.swing.JComboBox <String>();
 }
--- src/parkalot/set_car.java
+++ src/parkalot/set_car.java
@@ -16,6 +16,10 @@
  */
 package parkalot;
 
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.DefaultComboBoxModel;
+
 /**
  *
  * @author lll2yu
@@ -26,9 +30,59 @@ public class set_car extends javax.swing.JFrame {
      * Creates new form set_car
      */
     public set_car() {
+        add_box();
         initComponents();
     }
-
+    
+    ArrayList<String> list=new ArrayList<String>();
+    void add_box(){
+        get_car();
+        cb1.setModel(new DefaultComboBoxModel<String>(list.toArray(new String[0])));
+        getContentPane().add(cb1);
+        cb1.setBounds(280, 300, 160, 30);
+    }
+    void get_car(){
+        ResultSet rlt;
+        try{
+			Class.forName("add your jdbc Driver");
+			Connection c=DriverManager.getConnection("add your connectin info");
+			Statement stat=c.createStatement();
+			rlt=stat.executeQuery("Select name from rates ");			
+ 				while (rlt.next()) {      
+  					 list.add(rlt.getString(1));                                 
+				}
+		}
+		catch(Exception e){}
+    }
+    void add_car(){
+        String s1;
+        s1=tf1.getText();
+        ResultSet re;
+        try{
+			Class.forName("add your jdbc Driver");
+			Connection ce=DriverManager.getConnection("add your connectin info");
+			Statement se=ce.createStatement();
+			re=se.executeQuery("insert into rates(name) values('"+s1+"')");
+		}
+	catch(Exception e){}   
+    }
+    void remove_car(){
+        String s1;
+        Object sI=cb1.getSelectedItem();
+        if (sI != null)
+			{
+    				s1= sI.toString();
+                        }
+        s1=tf1.getText();
+        ResultSet re;
+        try{
+			Class.forName("add your jdbc Driver");
+			Connection ce=DriverManager.getConnection("add your connectin info");
+			Statement se=ce.createStatement();
+			re=se.executeQuery("delete from rates where name='"+s1+"'");
+		}
+	catch(Exception e){}  
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -38,21 +92,98 @@ public class set_car extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jDialog1 = new javax.swing.JDialog();
+        jLabel5 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        tf1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        jDialog1.setBounds(new java.awt.Rectangle(530, 240, 250, 50));
+        jDialog1.setMinimumSize(new java.awt.Dimension(350, 200));
+        jDialog1.setPreferredSize(new java.awt.Dimension(350, 200));
+        jDialog1.setResizable(false);
+        jDialog1.getContentPane().setLayout(null);
+
+        jLabel5.setText("Refresh the page");
+        jDialog1.getContentPane().add(jLabel5);
+        jLabel5.setBounds(120, 40, 140, 30);
+
+        jButton3.setText("OK");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        jDialog1.getContentPane().add(jButton3);
+        jButton3.setBounds(150, 120, 66, 29);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBounds(new java.awt.Rectangle(445, 120, 0, 0));
+        setMinimumSize(new java.awt.Dimension(500, 500));
+        setPreferredSize(new java.awt.Dimension(500, 500));
+        setResizable(false);
+        getContentPane().setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 1, 14)); // NOI18N
+        jLabel1.setText("Add/Remove Car Types");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(150, 30, 200, 30);
+
+        jLabel2.setText("Add Car Type");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(80, 110, 81, 30);
+
+        jLabel3.setText("Remove Car Type");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(80, 300, 105, 30);
+        getContentPane().add(tf1);
+        tf1.setBounds(280, 110, 160, 30);
+
+        jButton1.setText("ADD");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1);
+        jButton1.setBounds(200, 200, 90, 29);
+
+        jButton2.setText("REMOVE");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2);
+        jButton2.setBounds(200, 380, 90, 29);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/frame_bg_500.png"))); // NOI18N
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 0, 500, 500);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        add_car();
+        jDialog1.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        remove_car();
+        jDialog1.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        jDialog1.dispose();
+        new set_car().setVisible(true);
+        dispose();        
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -90,5 +221,16 @@ public class set_car extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JDialog jDialog1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField tf1;
     // End of variables declaration//GEN-END:variables
+    private javax.swing.JComboBox<String> cb1 = new javax.swing.JComboBox <String>();
 }
